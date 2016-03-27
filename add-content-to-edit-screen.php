2824fcdc0354637019d8ec653a34b8ec83e05b21
emersonthis <e@thesecretaddress.com>
2016-03-26 17:30:13 -0700

    Create add-content-to-edit-screen.php

---
+++ add-content-to-edit-screen.php
@@ -0,0 +1,6 @@
+# Adds instruction text after the post title input
+# https://make.wordpress.org/core/2012/12/01/more-hooks-on-the-edit-screen/
+add_action( 'edit_form_after_title', 'emersonthis_edit_form_after_title' );
+function emersonthis_edit_form_after_title() {
+    echo '<p style="margin-bottom:0;"><strong>TIP:</strong> To create a single line break use SHIFT+RETURN. By default, RETURN creates a new paragraph.</p>';
+}
