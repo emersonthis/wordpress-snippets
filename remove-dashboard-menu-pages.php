7c4a36c952dde6a3751c75d0cc6dee87fde09cea
emersonthis <e@thesecretaddress.com>
2016-03-30 17:12:18 -0700

    Update remove-dashboard-menu-pages.php

--- remove-dashboard-menu-pages.php
+++ remove-dashboard-menu-pages.php
@@ -1,4 +1,4 @@
-function custom_menu_page_removing() {
+function emersonthis_custom_menu_page_removing() {
   // remove_menu_page( 'index.php' );                  //Dashboard
   // remove_menu_page( 'jetpack' );                    //Jetpack* 
   remove_menu_page( 'edit.php' );                   //Posts
@@ -11,5 +11,4 @@ function custom_menu_page_removing() {
   // remove_menu_page( 'tools.php' );                  //Tools
   // remove_menu_page( 'options-general.php' );        //Settings
 }
-add_action( 'admin_menu', 'custom_menu_page_removing' );
-
+add_action( 'admin_menu', 'emersonthis_custom_menu_page_removing' );
