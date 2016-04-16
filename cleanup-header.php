4e24d961860f96a0907c8a6484004ef5caf28107
emersonthis <e@thesecretaddress.com>
2016-04-16 12:59:46 -0700

    Create cleanup-header.php

---
+++ cleanup-header.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Source: https://gist.github.com/HechtMediaArts/aceadf06b5e4431dc33d
+ */
+//emoji aus dem header entfernen
+function disable_emoji_dequeue_script() {
+    wp_dequeue_script( 'emoji' );
+}
+add_action( 'wp_print_scripts', 'disable_emoji_dequeue_script', 100 );
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 ); 
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+// Header Links entfernen
+add_action('init', 'remheadlink');
+function remheadlink()
+	{
+	remove_action('wp_head', 'rsd_link');
+	remove_action('wp_head', 'wp_generator');
+	remove_action('wp_head', 'index_rel_link');
+	remove_action('wp_head', 'wlwmanifest_link');
+	remove_action('wp_head', 'feed_links', 2);
+	remove_action('wp_head', 'feed_links_extra', 3);
+	remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+	remove_action('wp_head', 'start_post_rel_link', 10, 0);
+	remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+	remove_action('wp_head', 'wp_shortlink_header', 10, 0);
+	remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+	}
+	
