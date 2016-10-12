89f78df97568bc2b3f768b287a6c0168ad54e61b
emersonthis <e@thesecretaddress.com>
2016-10-12 12:29:22 -0700

    Update hide-mce-buttons.php

--- hide-mce-buttons.php
+++ hide-mce-buttons.php
@@ -2,10 +2,11 @@
  * Remove editor buttons
  * This probably belongs in your theme's functions.php file
  * Codex: http://codex.wordpress.org/Plugin_API/Filter_Reference/mce_buttons,_mce_buttons_2,_mce_buttons_3,_mce_buttons_4
+ * If you remove the kitchen sink button consider also adding: https://github.com/emersonthis/wordpress-snippets/blob/master/remove-second-row-tinymce-btns
  */
 function myplugin_tinymce_buttons($buttons)
  {
-    # Remove the text color selector
+    # Remove the kitchen sink button
     $remove = array('wp_adv');
     //To change other buttons, look at the button class names and remove the "mce-i-" prefix
 
