fe9da65e059dce10c83c3d6e5e2cdcc4df4513f2
emersonthis <e@thesecretaddress.com>
2018-02-21 23:52:19 -0500

    Create add-item-to-wp_nav_menu().php

---
+++ add-item-to-wp_nav_menu().php
@@ -0,0 +1,13 @@
+// Source: https://galengidman.com/2014/05/08/adding-static-menu-items-to-wp_nav_menu/
+function emersonthis_nav_wrap() {
+  // default value of 'items_wrap' is <ul id="%1$s" class="%2$s">%3$s</ul>'
+  // open the <ul>, set 'menu_class' and 'menu_id' values
+  $wrap  = '<ul id="%1$s" class="%2$s">';
+  // get nav items as configured in /wp-admin/
+  $wrap .= '%3$s';
+  // the static link 
+  $wrap .= '<li>NEW ITEM HERE</li>';
+  $wrap .= '</ul>';
+  // return the result
+  return $wrap;
+}
